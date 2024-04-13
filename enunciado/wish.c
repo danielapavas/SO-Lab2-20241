b25fbfcc7eb2fefdde9dcac31b6e6f1c09ef3540
giosof <giovani.ude@gmail.com>
2024-04-12 22:45:44 -0500

    Modo interactivo - Integración comandos integrados exit y cd

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -17,102 +17,88 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-
-#define MAX_LINE_LENGTH 1024
-#define MAX_ARGS 64
-#define MODE_BATCH 0
-#define MODE_INTERACTIVE 1
-int MODE;
+#define BUFFER_SIZE 1024
+#define HISTORY_SIZE 30
 
 // Declaracion de funciones
-void execute_command(char *args[], int background);
+void ejecutar_comando(char *command, char ***mypath);
 void interactive_mode();
 void batch_mode(char *batch_file);
 void ejecutar_exit(char *args);
 void ejecutar_cd(char *newpath);
 
 char error_message[30] = "An error has occurred\n";
+char history[HISTORY_SIZE][BUFFER_SIZE];
+int history_count = 0;
 
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        MODE = MODE_INTERACTIVE;
-        interactive_mode(); // No se proporcionó un archivo de lote, ejecutar en modo interactivo
-    } else if (argc == 2) {
-        MODE = MODE_BATCH;
-        batch_mode(argv[1]); // Se proporcionó un archivo de lote, ejecutar en modo batch
-    } else {
-        fprintf(stderr, "Usage: %s [batch_file]\n", argv[0]);
-        exit(1);
-    }
+    // Inicialización del mypath
+	char **mypath = malloc(2 * sizeof(char *));
+	mypath[0] = "/bin/";
+	mypath[1] = "";
 
-    return 0;
+	// Para capturar la entrada
+	char *input_line;
+
+	// Modo interactivo
+	if (argc == 1) {
+		do {
+			input_line = readline("wish> ");
+			
+            if (!input_line) {
+				// EOF o error
+				break;
+			}
+			
+            if (strlen(input_line) > 0){
+				add_history(input_line);
+
+				// Copiar cadena de comando al búfer de historial
+				if (history_count < HISTORY_SIZE) {
+					strcpy(history[history_count++], input_line);
+				} else {
+					for (int i = 0; i < HISTORY_SIZE - 1; i++) {
+						strcpy(history[i], history[i + 1]);
+					}
+					strcpy(history[HISTORY_SIZE - 1], input_line);
+				}
+			}
+
+			// Parseo de la entrada capturada con el fin de secuencia
+			input_line[strcspn(input_line, "\n")] = '\0';
+
+			// Ejecuto el comando
+			ejecutar_comando(input_line, &mypath);
+
+		} while (1);
+		free(input_line);
+	} else {
+		write(STDERR_FILENO, error_message, strlen(error_message));
+		return EXIT_FAILURE;
+	}
 }
 
-void interactive_mode() {
-    char input[MAX_LINE_LENGTH];
-    char *args[MAX_ARGS];
+void ejecutar_comando(char *command, char ***mypath) {
+    // Comandos Built-In, se validará que no se ingresen comandos como "cd /bin/ & path /bin/", ya que es incorrecto para comandos Built-In
+	if ((strstr(command, "cd") != 0 || strstr(command, "path") != 0 || strstr(command, "exit") != 0) && strstr(command, "&") == 0) {
 
-    while (1) {
-        // Print prompt
-        printf("wish> ");
-        fflush(stdout);
+		// Separamos del comando original el nombre del comando y sus argumentos
+		char *s = command;
+		char *command_string = strtok_r(s, " ", &s);
 
-        // Read input
-        if (fgets(input, MAX_LINE_LENGTH, stdin) == NULL) {
-            fprintf(stderr, "Error reading input\n");
-            exit(1);
-        }
-
-        // Remove newline character
-        input[strcspn(input, "\n")] = '\0';
-
-
-        // Execute the command
-        execute_command(args, 0);
-    }
+		if (strcmp(command_string, "exit") == 0)
+		{
+			ejecutar_exit(s);
+		}
+		else if (strcmp(command_string, "cd") == 0)
+		{
+			ejecutar_cd(s);
+		}
+	} else {
+		write(STDERR_FILENO, error_message, strlen(error_message));
+	}
 }
 
-void batch_mode(char *batch_file) {
-    FILE *file = fopen(batch_file, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening batch file\n");
-        exit(1);
-    }
-
-    char input[MAX_LINE_LENGTH];
-    char *args[MAX_ARGS];
-
-    while (fgets(input, MAX_LINE_LENGTH, file) != NULL) {
-        // Remove newline character
-        input[strcspn(input, "\n")] = '\0';
-
-
-        // Execute the command
-        execute_command(args, 1); // Run in background mode for batch mode
-    }
-
-    fclose(file);
-}
-
-void execute_command(char *args[], int background) {
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        fprintf(stderr, "Fork failed\n");
-        exit(1);
-    } else if (pid == 0) {
-        // Child process  
-        if (execvp(args[0], args) < 0) {
-            fprintf(stderr, "An error has occurred\n");
-            exit(1);
-        }
-    } else {
-        // Parent process
-        if (!background) {
-            waitpid(pid, NULL, 0); // Wait for child process to finish
-        }
-    }
-}
 void ejecutar_exit(char *args)
 {
 	char *path = strtok_r(args, " ", &args);
