8f44200f6198a5bc69ded3665ac7884a66b47898
daniela pavas <vboxuser@Ubuntu-Linux.myguest.virtualbox.org>
2024-04-12 22:12:17 -0500

    Eliminacion de archivo

--- enunciado/wish_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WISH_U_H_
-#define _WISH_U_H_
-
-void execute_exit(char *args);
-void execute_cd(char *newpath);
-void execute_path(char newpaths, char ***mypath);
-int wish_launch_redirect(char **args, char *file);
-
-#endif /* WISH_UTILS_H */
