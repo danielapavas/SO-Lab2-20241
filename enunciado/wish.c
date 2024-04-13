e47826325d21643bb43205dfac8dc6007fefb1a2
daniela pavas <vboxuser@Ubuntu-Linux.myguest.virtualbox.org>
2024-04-12 23:02:44 -0500

    Metodo ejecutar comando

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -19,6 +19,7 @@
 
 #define BUFFER_SIZE 1024
 #define HISTORY_SIZE 30
+#define MAX_SIZE 500
 
 // Declaracion de funciones
 void ejecutar_comando(char *command, char ***mypath);
@@ -79,6 +80,10 @@ int main(int argc, char *argv[]) {
 }
 
 void ejecutar_comando(char *command, char ***mypath) {
+
+    int num_com = 0;
+	int fd;
+
     // Comandos Built-In, se validará que no se ingresen comandos como "cd /bin/ & path /bin/", ya que es incorrecto para comandos Built-In
 	if ((strstr(command, "cd") != 0 || strstr(command, "path") != 0 || strstr(command, "exit") != 0) && strstr(command, "&") == 0) {
 
@@ -86,54 +91,197 @@ void ejecutar_comando(char *command, char ***mypath) {
 		char *s = command;
 		char *command_string = strtok_r(s, " ", &s);
 
-		if (strcmp(command_string, "exit") == 0)
-		{
+		if (strcmp(command_string, "exit") == 0){
 			ejecutar_exit(s);
 		}
-		else if (strcmp(command_string, "cd") == 0)
-		{
+		else if (strcmp(command_string, "cd") == 0){
 			ejecutar_cd(s);
 		}
+    else if (strstr(command, "cd") == NULL && strstr(command, "path") == NULL && strstr(command, "exit") == NULL){
+
+		// Extraccion de todos los posibles comandos
+		num_com = 0;
+		char *command_copy = strdup(command);
+		char *token = strtok(command_copy, "&");
+		while (token != NULL){
+			// Verificamos que el comando no esté vacío
+			token += strspn(token, " ");
+			if (*token != '\0'){
+				num_com++;
+			}
+			token = strtok(NULL, "&");
+		}
+
+		char *all_commands[num_com + 1];
+		char *command_copy2 = strdup(command);
+		token = strtok(command_copy2, "&");
+		int i = 0;
+		while (token != NULL){
+			/// Verificamos que el comando no esté vacío
+			token += strspn(token, " ");
+			if (*token != '\0'){
+				all_commands[i] = token;
+				// printf("Commands: %s\n",all_commands[i]);
+				i++;
+			}
+			token = strtok(NULL, "&");
+		}
+		all_commands[i] = NULL;
+
+		// Inicializamos los pids de los comandos capturados
+		pid_t pids[num_com];
+
+		// Recorrido para todos los comandos que serán lanzados
+		for (i = 0; i < num_com; i++){
+
+			// Separamos del comando original el nombre del comando y sus argumentos
+			char *s = all_commands[i];
+			char *command_string = strtok_r(s, " ", &s);
+
+			// Iniciamos comprobación del file descriptor basado en buscar las rutas del comando
+			fd = -1;
+			char ***mp_copy = malloc(2 * sizeof(char **)); // Crea una copia de mypath
+			memcpy(mp_copy, mypath, 2 * sizeof(char **));
+			char ***mp = mp_copy;
+			char specificpath[MAX_SIZE];
+
+			while ((strcmp((*mp)[0], "") != 0) && fd != 0){
+				strcpy(specificpath, *(mp[0]++));
+				strncat(specificpath, command_string, strlen(command_string));
+				fd = access(specificpath, X_OK);
+			}
+			// Si el file descriptor existe, osea la ruta del programa
+			if (fd == 0){
+				// Lanzaremos cada comando como un nuevo proceso
+				// En caso de error
+				if ((pids[i] = fork()) < 0){
+					perror("fork");
+					abort();
+
+				} // Proceso hijo creado
+				else if (pids[i] == 0){
+					// Como al hacer fork se hace una copia exacta e independiente de todo el programa padre, puedo manipular variables en el entorno del hijo
+					// Extraigo los argumentos para preparar el execv()
+
+					// Conteo de los argumentos del comando
+					int num_args = 0;
+					char *s_copy = strdup(s);
+					char *token = strtok(s_copy, " ");
+					while (token != NULL){
+						num_args++;
+						token = strtok(NULL, " ");
+					}
+
+					// Guardo los argumentos en el vector myargs
+					i = 1;
+					char *myargs[num_args + 1];
+					myargs[0] = strdup(command_string);
+					char *s_copy2 = strdup(s);
+					token = strtok(s_copy2, " ");
+					while (token != NULL){
+						myargs[i] = token;
+						token = strtok(NULL, " ");
+						i++;
+					}
+					myargs[i] = NULL;
+
+					// FUNCIÓN REDIRECCIÓN
+					int i_found = 0, aux = 0;
+					i = 0;
+					do{
+
+						if (strcmp(myargs[i], ">") == 0){
+							aux = aux + 1;
+							i_found = i;
+						}
+						else if (strchr(myargs[i], '>') != NULL){
+							aux = aux + 2;
+						}
+
+						i++;
+					} while (myargs[i] != NULL);
+
+					if (aux == 1){
+						// encontro 1 >
+						if (myargs[i_found + 1] == NULL){
+							write(STDERR_FILENO, error_message, strlen(error_message));
+						}
+						else if (myargs[i_found + 2] != NULL){
+							write(STDERR_FILENO, error_message, strlen(error_message));
+						}
+						else{
+							char *file = malloc(MAX_SIZE * sizeof(char *));
+							strcpy(file, myargs[i_found + 1]);
+							myargs[i_found] = NULL;
+							myargs[i_found + 1] = NULL;
+							if (file != NULL){
+
+								myargs[0] = strdup(specificpath);
+								redireccionar(myargs, file);
+							}
+							else{
+								write(STDERR_FILENO, error_message, strlen(error_message));
+							}
+						}
+					}
+					else if (aux > 1){
+						write(STDERR_FILENO, error_message, strlen(error_message));
+					}
+
+					else{
+
+						// Lanzo el proceso hijo como un nuevo programa
+						// Como me interesa ejecutar el proceso hijo como un nuevo programa, mando los argumentos capturados en el comando sobreescribiendo la imagen del proceso en cuestión
+						execv(specificpath, myargs);
+					}
+
+					// Si execv() es exitoso, lo siguiente nunca se ejecutará, por precaución para una salida segura
+					exit(EXIT_FAILURE);
+
+				} // Fin del proceso hijo
+			}	  // Si el file descriptor no existe
+			else{
+				write(STDERR_FILENO, error_message, strlen(error_message));
+			}
+		} // Fin del for
+
+		// Esperamos que todos los hijos terminen
+		int status;
+		pid_t pid;
+		for (i = 0; i < num_com; i++){
+			pid = wait(&status);
+		}
+	} // Comando incorrecto
 	} else {
 		write(STDERR_FILENO, error_message, strlen(error_message));
 	}
 }
 
-void ejecutar_exit(char *args)
-{
+void ejecutar_exit(char *args){
 	char *path = strtok_r(args, " ", &args);
-	if (path != NULL)
-	{
+	if (path != NULL){
 		write(STDERR_FILENO, error_message, strlen(error_message));
 	}
-	else
-	{
+	else{
 		exit(0);
 	}
 }
 
 // Implementación del comando cd para cambiar la ruta del directorio actual,sólo debe recibir 1 argumento que será el cambio a la nueva ruta
-void ejecutar_cd(char *newpath)
-{
+void ejecutar_cd(char *newpath){
 	char *path = strtok_r(newpath, " ", &newpath);
-	if (path == NULL)
-	{
+	if (path == NULL){
 		write(STDERR_FILENO, error_message, strlen(error_message));
 	}
-	else
-	{
-		if (strtok_r(NULL, " ", &newpath) != NULL)
-		{
+	else{
+		if (strtok_r(NULL, " ", &newpath) != NULL){
 			write(STDERR_FILENO, error_message, strlen(error_message));
 		}
-		else
-		{
-			if (access(path, F_OK) == 0) // Si la ruta existe
-			{
+		else{
+			if (access(path, F_OK) == 0){ // Si la ruta existe
 				chdir(path);
 			}
-			else
-			{
+			else{
 				write(STDERR_FILENO, error_message, strlen(error_message));
 			}
 		}
